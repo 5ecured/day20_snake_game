ac1baa3a0b12681c08ef33a1c66dd945fbccfac1
Edward <onirotto7585@pm.me>
2024-07-26 09:58:13 +0700

    added file system

--- scoreboard.py
+++ scoreboard.py
@@ -7,7 +7,8 @@ class Scoreboard(Turtle):
     def __init__(self):
         super().__init__()
         self.score = 0
-        self.high_score = 0
+        with open('data.txt') as data:
+            self.high_score = int(data.read())
         self.color('white')
         self.penup()
         self.goto(x=0, y=270)
@@ -22,13 +23,12 @@ class Scoreboard(Turtle):
         self.score += 1
         self.update_scoreboard()
 
-    # def game_over(self):
-    #     self.goto(x=0, y=0)
-    #     self.write(f"Game over", align=ALIGNMENT, font=FONT)
-
     def reset(self):
-        if self.score > self.high_score:
+        if self.score > int(self.high_score):
             self.high_score = self.score
+            with open('data.txt', mode='w') as data:
+                data.write(str(self.high_score))
+            
         self.score = 0
         
         self.update_scoreboard()
